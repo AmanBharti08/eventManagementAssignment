27eaf62f8a26b74db9252b82d27595570941e7d3
Aman <amanbharti0897@gmail.com>
2026-01-05 11:16:02 +0530

    ok

--- server.js
+++ server.js
@@ -14,13 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://event-management-ffrontend.vercel.app/'  // Add your Vercel URL here
-  ],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
